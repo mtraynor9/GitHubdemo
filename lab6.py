ebd302ba10b68c39a90aa035534ecf8acc4f2c3d
Caio <csmiyake@ufl.edu>
2024-10-23 13:59:35 -0400

    Final Edits Completed! Project completed!

--- lab6.py
+++ lab6.py
@@ -13,9 +13,13 @@ def encode(password):
     # returns modified pass word
     return encoded_pass
 
-#Placeholder for team member
+#This is the decoded function!
 def decode(encoded_password):
-    pass
+    decoded_pass = " "
+    for char in encoded_password:
+        char = int(char) - 3
+        decoded_pass += str(char)
+    return decoded_pass
 
 
 
@@ -23,10 +27,10 @@ def decode(encoded_password):
 Continue = True
 #While loop
 while Continue:
-    print('Menu \n------------- \
+    print('\nMenu \n------------- \
             \n1. Encode  \
             \n2. Decode \
-            \n3. Quit ')
+            \n3. Quit\n ')
 
     option = int(input('Please enter an option: '))
 
@@ -34,11 +38,16 @@ while Continue:
         password = input('Enter your password to encode: ')
         # here you encode your password and store it!
         encoded_password = encode(password)
+        #password encoded statement:
+        print('Your password has been encoded and stored!')
 
-        pass
+
     if option ==2:
-        pass
+        original_pass = decode(encoded_password)  # Sets original_pass to decoded version of the encoded password
+        print(f'The encoded password is {encoded_password}, and the original password is {original_pass}.')  # string allows looped function
+
     if option == 3:
+        #terminates the loop
         Continue = False
 
 
